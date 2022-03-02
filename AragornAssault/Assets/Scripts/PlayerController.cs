d639818208a27a092515fbfaab0f07f2d02d1312
TheDarkestNightRises <91905169+TheDarkestNightRises@users.noreply.github.com>
2022-03-02 11:47:27 +0100

    Fixed bug with player Movement

--- AragornAssault/Assets/Scripts/PlayerController.cs
+++ AragornAssault/Assets/Scripts/PlayerController.cs
@@ -9,7 +9,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float xRange = 10f;
     [SerializeField] float yRange = 7f;
     // Start is called before the first frame update
+    [SerializeField] float pitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -15f;
+    [SerializeField] float controlYawFactor = 2f;
+    [SerializeField] float controlRollFactor = 5f;
 
+    float horizontalThrow;
+    float verticalThrow;
 
     private void OnEnable()
     {
@@ -21,20 +27,39 @@ public class PlayerController : MonoBehaviour
         movement.Disable();
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = movement.ReadValue<Vector2>().x;
-        float verticalThrow = movement.ReadValue<Vector2>().y;
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessTranslation()
+    {
+        horizontalThrow = movement.ReadValue<Vector2>().x;
+        verticalThrow = movement.ReadValue<Vector2>().y;
 
         float xOffset = horizontalThrow * Time.deltaTime * controlSpeed;
         float rawXpos = transform.localPosition.x + xOffset;
-        float clampedXpos = Mathf.Clamp(rawXpos,-xRange,xRange);
+        float clampedXpos = Mathf.Clamp(rawXpos, -xRange, xRange);
 
         float yOffset = verticalThrow * Time.deltaTime * controlSpeed;
         float rawYpos = transform.localPosition.y + yOffset;
-        float clampedYpos = Mathf.Clamp(rawYpos,-yRange,yRange);
+        float clampedYpos = Mathf.Clamp(rawYpos, -yRange, yRange);
 
-        transform.localPosition = new Vector3 (clampedXpos,clampedYpos,transform.localPosition.z);
+        transform.localPosition = new Vector3(clampedXpos, clampedYpos, transform.localPosition.z);
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * pitchFactor;
+        float pitchDueToControlThrow = +verticalThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yaw = transform.localPosition.x * controlYawFactor;
+        float roll = horizontalThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
