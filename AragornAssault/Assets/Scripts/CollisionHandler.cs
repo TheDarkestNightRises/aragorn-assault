008f65c85d56bd3d80f67ded8ca02ad29e5ca9f2
TheDarkestNightRises <91905169+TheDarkestNightRises@users.noreply.github.com>
2022-03-02 16:11:47 +0100

    Collision for player reworked

--- AragornAssault/Assets/Scripts/CollisionHandler.cs
+++ AragornAssault/Assets/Scripts/CollisionHandler.cs
@@ -1,16 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision other)
-    {
-        Debug.Log(this.name + "collided with " + other.gameObject.name);
-    }
 
+    [SerializeField] float loadDelay = 1f;
     private void OnTriggerEnter(Collider other)
     {
+        StartCrashSequence();
+    
         Debug.Log($"{this.name} **Triggerd by** {other.gameObject.name}");
+    }
+
+    private void StartCrashSequence()
+    {
+        GetComponent<PlayerController>().enabled = false;
+        Invoke("ReloadLevel", loadDelay);
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
