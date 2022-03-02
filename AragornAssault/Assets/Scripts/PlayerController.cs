7366f51e44375e5c09635142dd639c6c264efd67
TheDarkestNightRises <91905169+TheDarkestNightRises@users.noreply.github.com>
2022-03-02 10:48:38 +0100

    Added PlayerRotation

--- AragornAssault/Assets/Scripts/PlayerController.cs
+++ AragornAssault/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float controlSpeed;
     [SerializeField] float xRange = 10f;
     [SerializeField] float yRange = 7f;
+
+    [SerializeField] float pitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -15f;
+    [SerializeField] float controlYawFactor = 2f;
+    [SerializeField] float controlRollFactor = 5f;
+
+    float horizontalThrow;
+    float verticalThrow;
     // Start is called before the first frame update
 
 
@@ -24,17 +33,35 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = movement.ReadValue<Vector2>().x;
-        float verticalThrow = movement.ReadValue<Vector2>().y;
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * pitchFactor;
+        float pitchDueToControlThrow =  + verticalThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yaw = transform.localPosition.x * controlYawFactor;
+        float roll = horizontalThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+         horizontalThrow = movement.ReadValue<Vector2>().x;
+         verticalThrow = movement.ReadValue<Vector2>().y;
 
         float xOffset = horizontalThrow * Time.deltaTime * controlSpeed;
         float rawXpos = transform.localPosition.x + xOffset;
-        float clampedXpos = Mathf.Clamp(rawXpos,-xRange,xRange);
+        float clampedXpos = Mathf.Clamp(rawXpos, -xRange, xRange);
 
         float yOffset = verticalThrow * Time.deltaTime * controlSpeed;
         float rawYpos = transform.localPosition.y + yOffset;
-        float clampedYpos = Mathf.Clamp(rawYpos,-yRange,yRange);
+        float clampedYpos = Mathf.Clamp(rawYpos, -yRange, yRange);
 
-        transform.localPosition = new Vector3 (clampedXpos,clampedYpos,transform.localPosition.z);
+        transform.localPosition = new Vector3(clampedXpos, clampedYpos, transform.localPosition.z);
     }
 }
