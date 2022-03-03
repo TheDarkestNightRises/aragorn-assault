d62387f16ca9773d3a09dc52abd94407358850f4
TheDarkestNightRises <91905169+TheDarkestNightRises@users.noreply.github.com>
2022-03-03 20:35:34 +0100

    Added Dialogue for game

--- AragornAssault/Assets/Scripts/Enemy.cs
+++ AragornAssault/Assets/Scripts/Enemy.cs
@@ -6,17 +6,19 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] GameObject hitVFX;
-    [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 15;
     [SerializeField] int hitPoints = 4;
 
     ScoreBoard scoreBoard;
+    GameObject parentGameObject;
+
 
     private void Start()
     {
         // Resource heavy for update... not so for start
         AddRigidBody();
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
     }
 
     private void AddRigidBody()
@@ -38,15 +40,16 @@ public class Enemy : MonoBehaviour
     private void KillEnemy(GameObject other)
     {
         Debug.Log($"{other.gameObject.name} Shoot {name}");
+        scoreBoard.IncreaseScore(scorePerHit);
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         Destroy(gameObject);
     }
 
     private void ProcessHit()
     {
         GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         hitPoints--;
         scoreBoard.IncreaseScore(scorePerHit);
     }
